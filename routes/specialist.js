3aeef6a213a85649fdc8f90458b8838103012b94
jonathan122 <jonathan.christipherw@gmail.com>
2022-04-10 23:47:04 +0100

    starting the specialist page development. Attempting to use node to connect to database

--- routes/specialist.js
+++ routes/specialist.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const data = require('../database')
 const router = express.Router()
+
+var problemType = [];
+for (let i = 0; i < data.length; i++) {
+    var problem_type = data[i].maintag;
+    if (!problemType.includes(problem_type)) {
+        problemType.push(problem_type);
+    }
+}
+console.log(problemType);
 
 router
     .get('/', (req, res) => {
-        res.send("Specialist Main")
+        res.render('specialist', { data: { data: data, problemType: problemType } })
     })
     .get('/analyst', (req, res) => {
         res.send("Specialist Analyst")
-    })  
+    })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+// var mysql = require('mysql');
+// var conn = mysql.createConnection({
+//     host: "localhost",
+//     user: "teamb029",
+//     password: "pXdBPQK4cL",
+//     database: "teamb029",
+// });
+
+// conn.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+
+//     connection.query('SELECT * FROM ticket', (err, rows) => {
+//         connection.release()
+
+//         if (!err) {
+//             res.send(rows)
+//         } else {
+//             console.log(err)
+//         }
+//     })
+// })
+// module.exports = conn;
+
 const express = require('express')
 const app = express()
+const data = require('./database')
 
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
@@ -7,8 +32,9 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    console.log("Here")
-    res.render("index", {text: 'Test Text'})
+    console.log(data)
+    res.render("index", { text: 'Test 44' })
+
 })
 
 const userRouter = require('./routes/user')
@@ -17,4 +43,6 @@ app.use('/users', userRouter)
 const specRouter = require('./routes/specialist')
 app.use('/specialist', specRouter)
 
-app.listen(3000)
+app.listen(3306)
+
+// npm run devStart
