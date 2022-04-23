9adb3805cf8e8638d1d158bc469c2623ac67f8c6
arda1228 <92720325+arda1228@users.noreply.github.com>
2022-04-23 17:41:20 +0100

    test

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const router = express.Router()
+let resultObject = {result:[]}
 
 router.use(logger)
 
 router.get('/', (req, res) => {
-    res.render("user/main")
+    res.render("user/main", resultObject)
+})
+
+router.post('/searched', (req, res) => {
+// doing a db search on inputted text object
+    //grab search term
+    let searchTerm = req.body.searchbar
+    console.log("search term: "+ searchTerm)
+    //init db query with search term
+    //pretend we got a db, make results object
+    //list results in charlotte's js in main or partial
+    //get results and make a results object
+    let resultObject = {result:[{issue: "printer", resolution: "restart"}, {issue: "computer", resolution: "off and on again"}]}
+    res.render("user/main", resultObject) // FIX
 })
 
 router.get('/new', (req, res) => {
@@ -17,11 +31,11 @@ router.get('/history', (req, res) => {
 
 router.get("/contact", (req, res) => {
     res.render("user/contact")
-  })
+})
 
 router.get("/active-issues", (req, res) => {
     res.render("user/active-issues")
-  })
+})
 
 router.post("/", (req, res) => {
     const isValid = true
@@ -35,7 +49,7 @@ router.post("/", (req, res) => {
 })
 
 router
-.route("/:id")
+    .route("/:id")
     .get((req, res) => {
         console.log(req.user)
         res.send(`Get User With ID ${req.params.id}`)
@@ -45,10 +59,10 @@ router
     })
     .delete((req, res) => {
         res.send(`Delete User With ID ${req.params.id}`)
-})
+    })
 
 const users = [{ name: "Kyle" }, { name: "Sally" }]
-    router.param("id", (req, res, next, id) => {
+router.param("id", (req, res, next, id) => {
     req.user = users[id]
     next()
 })
@@ -59,7 +73,7 @@ function logger(req, res, next) {
 }
 
 module.exports = router
-  
+
 /*login
 usermain
 userspec
