6957e784effb98a8b49d97d2175d14b4cb0284b5
arda1228 <92720325+arda1228@users.noreply.github.com>
2022-05-02 15:34:00 +0100

    shows searches and solutions

--- routes/user.js
+++ routes/user.js
@@ -10,41 +10,32 @@ var con = mysql.createConnection({
     database: "teamb029",
     multipleStatements: true
 });
+con.connect(function(err) {
+    if (err) throw err; 
+})
 
 router.use(logger)
 
 router.get('/', (req, res) => {
-        con.connect(function(err) {
-            if (err) throw err;
             con.query("SELECT DISTINCT TagTable.tagName FROM Ticket LEFT JOIN `TagTable` ON  Ticket.mainTag = TagTable.ID;", function(err, result, fields) {
                 if (err) throw err;
                 res.render('user/main', { data: result, source: "initialEntry" })
 
             });
-        });
 
 })
 
 router.post('/searched', (req, res) => {
-// doing a db search on inputted text object
-    //grab search term
     let searchTerm = req.body.searchbar
+    let myQuery = `SELECT ticketDescription, resolvedDescription FROM Ticket WHERE ticketState = "RESOLVED" AND ((ticketDescription  LIKE '%`+searchTerm.toLowerCase()+`%' OR resolvedDescription LIKE '%`+searchTerm.toLowerCase()+`%') OR (ticketDescription  LIKE '%`+searchTerm+`%' OR resolvedDescription LIKE '%`+searchTerm+`%'))`
     let resultObject
-    if (searchTerm == "printer" || "printer jam") {
-        console.log("search term: "+ searchTerm)
-        //init db query with search term
-    
-        //pretend we got a db, make results object
-        //list results in charlotte's js in main or partial
-        //get results and make a results object
-        resultObject = {data:[{issue: "printer", resolution: "restart"}, {issue: "computer", resolution: "off and on again"}]}
-    }
-    if (searchTerm == "bottle") {
-        console.log("search term: "+ searchTerm)
-        resultObject = {data:[{issue: "broken bottle", resolution: "kick"}, {issue: "hello", resolution: "goodbye"}]}
-    }
-    res.render("user/main", {resultObject,  source: "searchEntry"} ) // FIX
+    con.query(myQuery, function(err, result, fields) {
+        if (err) throw err;
+        result.map(el=>console.log(el))
+        res.render('user/main', { data: result, source: "searchEntry" })
+    });
 })
+
 
 router.post('/maintag', (req, res) => {
     console.log("LOOK AT ME")
